cde1ebda0411dbd58b701b0543d0f4e98b94e195
wzm <k893424088@gmail.com>
2022-12-09 18:17:21 +0900

    add linearRegression with L2norm

--- src/LinearRegressionMain_L2norm.py
+++ src/LinearRegressionMain_L2norm.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.dont_write_bytecode = True
-from Regression.LinearRegression import LinearRegression as lr
+from Regression.LinearRegression import LinearRegression_L2norm as lr
 
 import data
 
@@ -17,11 +17,11 @@ Ytr = myData.Y[:dtrNum]
 Xte = myData.X[dtrNum:]
 Yte = myData.Y[dtrNum:]
 
-myModel = lr.linearRegression(Xtr,Ytr)
-myModel.train()
+myModel = lr.linearRegression_L2norm(Xtr,Ytr)
+myModel.trainRegularized(lamb=0.5)
 
 print(f"model parameter:\nw={myModel.w},\nb={myModel.b}")
 print(f"RMSE=${myModel.RMSE(Xte,Yte):.2f}")
 print(f"coefficient of determination={myModel.R2(Xte,Yte):.2f}")
 
-myModel.plotResult(X=Xtr,Y=Ytr,xLabel=myData.xLabel,yLabel=myData.yLabel,fName=f"../results/linearRegression_result_train_{myData.dataType}.pdf")
+myModel.plotResult(X=Xtr,Y=Ytr,xLabel=myData.xLabel,yLabel=myData.yLabel,fName=f"../results/linearRegression_L2norm_result_train_{myData.dataType}.pdf")
---
+++ src/Regression/LinearRegression/LinearRegression_L2norm.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+class linearRegression_L2norm():
+    def __init__(self,X,Y):
+        self.X = X
+        self.Y = Y
+        self.dNum = X.shape[0]
+        self.xDim = X.shape[1]
+
+    def trainRegularized(self,lamb=0.5):
+        Z = np.concatenate([self.X,np.ones([self.dNum,1])],axis=1)
+        ZZ = 1/self.dNum * np.matmul(Z.T,Z) + lamb*np.eye(self.xDim)
+        ZY = 1/self.dNum * np.matmul(Z.T,self.Y)
+        v = np.matmul(np.linalg.inv(ZZ),ZY)
+        self.w = v[:-1]
+        self.b = v[-1]
+
+    def predict(self,x):
+        return np.matmul(x,self.w) + self.b
+
+    def RMSE(self,X,Y):
+        return np.sqrt(np.mean(np.square(self.predict(X)-Y)))
+
+    def R2(self,X,Y):
+        a = np.sum(np.square(self.predict(X)-Y))
+        b = np.sum(np.square(Y-np.mean(Y,axis=0)))
+        return 1-a/b
+
+    def plotResult(self,X=[],Y=[],xLabel="",yLabel="",fName=""):
+        if X.shape[1] != 1: 
+            return
+        
+        fig = plt.figure(figsize=(8,5),dpi=100)
+
+        Xlin = np.array([[0],[np.max(X)]])
+        Yplin = self.predict(Xlin)
+
+        plt.plot(X,Y,'.',label="data")
+        plt.plot(Xlin,Yplin,'r',label="linear model")
+        plt.legend()
+
+        plt.ylim([0,np.max(Y)])
+        plt.xlim([0,np.max(X)])
+        plt.xlabel(xLabel)
+        plt.ylabel(yLabel)
+
+        if len(fName):
+            plt.savefig(fName)
+        plt.show()
