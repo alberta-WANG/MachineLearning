9f4ff81061d047e66dbb6bad0204361a1b98b8ec
wzm <k893424088@gmail.com>
2022-12-09 17:37:08 +0900

    update linearRegression and result

---
+++ src/LinearRegressionMain.py
@@ -0,0 +1,24 @@
+import os
+from Regression.LinearRegression import LinearRegression as lr
+import data
+
+
+myData = data.regression()
+myData.makeData(dataType = 1)
+
+dtrNum = int(len(myData.X)*0.9)
+
+Xtr = myData.X[:dtrNum]
+Ytr = myData.Y[:dtrNum]
+
+Xte = myData.X[dtrNum:]
+Yte = myData.Y[dtrNum:]
+
+myModel = lr.linearRegression(Xtr,Ytr)
+myModel.train()
+
+print(f"model parameter:\nw={myModel.w},\nb={myModel.b}")
+print(f"RMSE=${myModel.RMSE(Xte,Yte):.2f}")
+print(f"coefficient of determination={myModel.R2(Xte,Yte):.2f}")
+
+myModel.plotResult(X=Xtr,Y=Ytr,xLabel=myData.xLabel,yLabel=myData.yLabel,fName=f"../results/linearRegression_result_train_{myData.dataType}.pdf")
